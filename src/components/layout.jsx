756e00af5cab8d06a3039f9118081c1696ad918b
Ashutosh Tiwari <ashutoshtiwari8840@gmail.com>
2023-04-24 20:56:52 +0530

    Initial commit

---
+++ src/components/layout.jsx
@@ -0,0 +1,14 @@
+import Header from "./header/header";
+import Footer from "./footer/footer";
+
+const Layout = ({ children }) => {
+	return (
+		<>
+			<Header />
+			<main>{children}</main>
+			<Footer />
+		</>
+	);
+};
+
+export default Layout;
---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Home() {
+	return (
+		<>
+			<Head>
+				<title>Sneaker Store</title>
+				<meta name="description" content="An online sneaker marketplace" />
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+		</>
+	);
+}
