c0b0bf7936e958b01bfdbb0ab23b271e80e0b873
Ashutosh Tiwari <ashutoshtiwari8840@gmail.com>
2023-04-29 03:00:31 +0530

    Font optimisation

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,15 +1,20 @@
 import { Provider } from "react-redux";
 import Layout from "@/components/layout";
 import { wrapper } from "../redux/store";
+import { Nunito } from "next/font/google";
 import "../styles/globals.scss";
+
+const nunito = Nunito({ subsets: ["latin"] });
 
 export default function App({ Component, ...rest }) {
 	const { store, props } = wrapper.useWrappedStore(rest);
 	return (
 		<Provider store={store}>
-			<Layout>
-				<Component {...props.pageProps} />
-			</Layout>
+			<main className={nunito.className}>
+				<Layout>
+					<Component {...props.pageProps} />
+				</Layout>
+			</main>
 		</Provider>
 	);
 }
