bc8dc35549109e14bcdabc99c63f5e7dc27a2c7c
Ashutosh Tiwari <ashutoshtiwari8840@gmail.com>
2023-06-10 13:55:06 +0530

    Update index.jsx

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,18 +1,25 @@
 import { useEffect } from "react";
 import { useRouter } from "next/router";
 import { useDispatch, useSelector } from "react-redux";
-import { Grid } from "@mui/material";
+import { Grid, IconButton } from "@mui/material";
 import styles from "./index.module.scss";
 import ProductTile from "../../components/home/product-tile/product-tile";
 import { getProductList } from "@/redux/actions/home";
 import Spinner from "@/components/common/spinner/spinner";
 import HeroBanner from "@/components/home/hero-banner/hero-banner";
+import Filter from "@/components/home/filter/filter";
+import useBreakpoint from "@/utils/hooks/useBreakpoint";
+import { GridViewOutlined, ViewAgendaOutlined } from "@mui/icons-material";
+import { toggleMobileGrid } from "@/redux/actions/ui";
 
 const Home = () => {
   const router = useRouter();
   const dispatch = useDispatch();
+  const breakpoint = useBreakpoint();
+
   const productList = useSelector((state) => state?.home?.list ?? []);
   const isLoading = useSelector((state) => state?.ui?.isLoading ?? false);
+  const mobileGrid = useSelector((state) => state?.ui?.mobileGrid ?? false);
 
   useEffect(() => {
     dispatch(getProductList());
@@ -28,23 +35,39 @@ const Home = () => {
     }
   };
 
+  const onChangeViewClick = () => {
+    dispatch(toggleMobileGrid(!mobileGrid));
+  };
+
   return (
     <>
       {isLoading ? (
         <Spinner />
       ) : (
         <div className={styles["container"]}>
-          <HeroBanner />
-          <div className={styles["product-container"]}>
-            <Grid container spacing={1}>
-              {productList.map((item, index) => (
-                <ProductTile
-                  onClick={onProductTileClick}
-                  key={index}
-                  item={item}
-                />
-              ))}
-            </Grid>
+          <div>
+            <HeroBanner />
+          </div>
+          <div className={styles["product-filter-wrapper"]}>
+            <div className={styles["filter-container"]}>
+              <Filter />
+              {breakpoint === "xs" && (
+                <IconButton onClick={onChangeViewClick}>
+                  {mobileGrid ? <GridViewOutlined /> : <ViewAgendaOutlined />}
+                </IconButton>
+              )}
+            </div>
+            <div className={styles["product-container"]}>
+              <Grid container spacing={1}>
+                {productList.map((item, index) => (
+                  <ProductTile
+                    onClick={onProductTileClick}
+                    key={index}
+                    item={item}
+                  />
+                ))}
+              </Grid>
+            </div>
           </div>
         </div>
       )}
