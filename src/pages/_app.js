84cdae4981ebc4f25bdaf9a09a59fa7408b3ef0f
Ashutosh Tiwari <ashutoshtiwari8840@gmail.com>
2023-06-10 15:20:22 +0530

    Updated font implementation

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,17 +1,15 @@
 import { Provider } from "react-redux";
 import Layout from "@/components/layout";
 import { wrapper } from "../redux/store";
-import { Nunito, Bebas_Neue } from "next/font/google";
+import "@fontsource/bebas-neue";
+import "@fontsource/roboto";
 import "../styles/globals.scss";
-
-export const nunito = Nunito({ subsets: ["latin"] });
-export const bebas = Bebas_Neue({ subsets: ["latin"], weight: ["400"] });
 
 export default function App({ Component, ...rest }) {
   const { store, props } = wrapper.useWrappedStore(rest);
   return (
     <Provider store={store}>
-      <main className={nunito.className}>
+      <main>
         <Layout>
           <Component {...props.pageProps} />
         </Layout>
