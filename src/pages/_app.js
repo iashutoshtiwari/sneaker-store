c6327f5ab6e33e4ae5a94ea35cfaf0e762cd7142
Ashutosh Tiwari <ashutoshtiwari8840@gmail.com>
2023-06-10 14:59:27 +0530

    Added Bebas Neue font

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,20 +1,21 @@
 import { Provider } from "react-redux";
 import Layout from "@/components/layout";
 import { wrapper } from "../redux/store";
-import { Nunito } from "next/font/google";
+import { Nunito, Bebas_Neue } from "next/font/google";
 import "../styles/globals.scss";
 
-const nunito = Nunito({ subsets: ["latin"] });
+export const nunito = Nunito({ subsets: ["latin"] });
+export const bebas = Bebas_Neue({ subsets: ["latin"], weight: ["400"] });
 
 export default function App({ Component, ...rest }) {
-	const { store, props } = wrapper.useWrappedStore(rest);
-	return (
-		<Provider store={store}>
-			<main className={nunito.className}>
-				<Layout>
-					<Component {...props.pageProps} />
-				</Layout>
-			</main>
-		</Provider>
-	);
+  const { store, props } = wrapper.useWrappedStore(rest);
+  return (
+    <Provider store={store}>
+      <main className={nunito.className}>
+        <Layout>
+          <Component {...props.pageProps} />
+        </Layout>
+      </main>
+    </Provider>
+  );
 }
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,15 +1,23 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
-	return (
-		<Html lang="en">
-			<Head />
-			<body>
-				<Main />
-				<NextScript />
-			</body>
-			<script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js" async />
-			<script noModule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js" async />
-		</Html>
-	);
+  return (
+    <Html lang="en">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+      <script
+        type="module"
+        src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"
+        async
+      />
+      <script
+        noModule
+        src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"
+        async
+      />
+    </Html>
+  );
 }
