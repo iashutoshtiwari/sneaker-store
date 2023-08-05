82c3054052f8b8e96a1ab453bf2aed6982985ac5
Ashutosh Tiwari <ashutoshtiwari8840@gmail.com>
2023-08-05 10:35:04 +0530

    Update product-images.jsx

--- src/components/pdp/product-images/product-images.jsx
+++ src/components/pdp/product-images/product-images.jsx
@@ -6,58 +6,58 @@ import useBreakpoint from "@/utils/hooks/useBreakpoint";
 import { TransformComponent, TransformWrapper } from "react-zoom-pan-pinch";
 
 const ProductImages = () => {
-	const item = useSelector((state) => state?.pdp?.details);
+  const item = useSelector((state) => state?.pdp?.details);
 
-	const breakpoint = useBreakpoint();
+  const breakpoint = useBreakpoint();
 
-	//Carousel settings
-	const settings = {
-		dots: false,
-		arrows: breakpoint === "md" || breakpoint === "lg" || breakpoint === "xl" ? true : false,
-		infinite: true,
-		speed: 500,
-		slidesToShow: 1,
-		slidesToScroll: 1,
-	};
+  //Carousel settings
+  const settings = {
+    dots: false,
+    arrows: breakpoint !== "xs" && breakpoint !== "sm" ? true : false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
 
-	return (
-		<div className={styles["product-images-container"]}>
-			<Carousel settings={settings}>
-				{item.images &&
-					item.images.map((image) => {
-						return breakpoint === "xs" || breakpoint === "sm" ? (
-							<TransformWrapper key={item?.id} panning={{ disabled: true }}>
-								<TransformComponent>
-									<div className={styles["next-image-wrapper"]}>
-										<Image
-											sizes="100vw"
-											fill
-											style={{ objectFit: "contain" }}
-											alt={item?.name}
-											src={image}
-											priority
-											unoptimized
-										/>
-									</div>
-								</TransformComponent>
-							</TransformWrapper>
-						) : (
-							<div className={styles["next-image-wrapper"]}>
-								<Image
-									sizes="100vw"
-									fill
-									style={{ objectFit: "contain" }}
-									alt={item?.name}
-									src={image}
-									priority
-									unoptimized
-								/>
-							</div>
-						);
-					})}
-			</Carousel>
-		</div>
-	);
+  return (
+    <div className={styles["product-images-container"]}>
+      <Carousel settings={settings}>
+        {item.images &&
+          item.images.map((image) => {
+            return breakpoint === "xs" || breakpoint === "sm" ? (
+              <TransformWrapper key={item?.id} panning={{ disabled: true }}>
+                <TransformComponent>
+                  <div className={styles["next-image-wrapper"]}>
+                    <Image
+                      sizes="100vw"
+                      fill
+                      style={{ objectFit: "contain" }}
+                      alt={item?.name}
+                      src={image}
+                      priority
+                      unoptimized
+                    />
+                  </div>
+                </TransformComponent>
+              </TransformWrapper>
+            ) : (
+              <div className={styles["next-image-wrapper"]}>
+                <Image
+                  sizes="100vw"
+                  fill
+                  style={{ objectFit: "contain" }}
+                  alt={item?.name}
+                  src={image}
+                  priority
+                  unoptimized
+                />
+              </div>
+            );
+          })}
+      </Carousel>
+    </div>
+  );
 };
 
 export default ProductImages;
